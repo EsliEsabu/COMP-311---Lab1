f1803d311148afc8fbdac8c4472eb86fd76978ee
Esli Esabu <EE24019733@BIUST.AC.BW>
2026-08-30 19:36:17 +0200

    add Question 2 : Primitive Playground

---
+++ Question2.java
@@ -0,0 +1,24 @@
+public class Question2 {
+    public static void main (String [] args){
+
+        byte bvalue = 100;
+        short svalue = 20000;
+        int ivalue = 350000;
+        long lvalue = 900000000;
+        float fvalue = 3.14f;
+        double dvalue = 3.14159265;
+        char cvalue = 'E';
+        boolean boolvalue = true;
+
+        System.out.println("The byte value is: " + bvalue);
+        System.out.println( "The short value is: " + svalue);
+        System.out.println("The int value is: " + ivalue );
+        System.out.println("The long value is: " + lvalue);
+        System.out.println("The float value is: " + fvalue);
+        System.out.println("The double value is: " + dvalue);
+        System.out.println("The char value is: " + cvalue);
+        System.out.println("The boolean value is: " + boolvalue);
+
+    }
+    
+}
