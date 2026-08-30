996e45cdcfca37965f18c6096f74889aa84506bc
Esli Esabu <EE24019733@BIUST.AC.BW>
2026-08-30 19:04:23 +0200

    Add Question 1: Personal Profile

---
+++ Question1.java
@@ -0,0 +1,15 @@
+public class Question1 {
+    public static void main (String [] args){
+String name = "Quest" ;
+int age = 20;
+double gpa = 4.0;
+boolean fulltime = true;
+
+       System.out.println("My name is:" + name);
+       System.out.println("I am:" + age);
+       System.out.println( "My GPA is:" + gpa);
+       System.out.println("I am a fulltime student:" + fulltime);
+
+    
+    }
+}
