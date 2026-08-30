c51e3e050ff5072f09eeb2296893d5faa4fe3650
Esli Esabu <EE24019733@BIUST.AC.BW>
2026-08-30 23:46:11 +0200

    Add Question8: Rectangle Calculator

---
+++ Question8.java
@@ -0,0 +1,29 @@
+//Esli Quest Esabu 
+//24019733
+//Question 7
+
+import java.util.Scanner;
+
+public class Question8 {
+    public static void main (String [] args){
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter length: ");
+        double length = sc.nextDouble();
+
+        System.out.println("Enter width: ");
+        double width = sc.nextDouble();
+
+        double area = length * width;
+        double perimeter = 2 * (length + width);
+
+        System.out.println("Area: " + area);
+        System.out.println("Perimeter: " + perimeter);
+
+
+
+        sc.close();
+    }
+    
+}
