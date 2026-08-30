45861c0fc100167371fc06a7befab514c001caef
Esli Esabu <EE24019733@BIUST.AC.BW>
2026-08-30 22:35:04 +0200

    Add Question3

---
+++ Question3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Question3 {
+    public static void main (String [] args){
+
+        try(Scanner sc = new Scanner(System.in)){
+
+        System.out.print("Enter name: ");
+        String name = sc.nextLine();
+
+        System.out.println("Enter age: ");
+        int age = sc.nextInt();
+
+        System.out.println("Hello " + name + "! In five years you will be " + (age + 5) + "years old." );
+
+        sc.close();
+        }
+
+    }
+    
+}
